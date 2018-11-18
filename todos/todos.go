1d749e643c721f674c5c020e6086687098864af0
veith <veithz@gmail.com>
2018-11-18 13:02:46 +0100

    hateoas on collection

--- todos/todos.go
+++ todos/todos.go
@@ -2,21 +2,24 @@ package todos
 
 import (
 	"../protos"
+	"github.com/oklog/ulid"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"math/rand"
 	"strconv"
+	"time"
 	"upper.io/db.v3"
 )
 
 // Gib mir eine DB Session und du bekommst die impl
 func Register(database db.Database) todo.TodoServiceServer {
-	todoCollection = database.Collection("todo")
+	dbCollectionTodo = database.Collection("todo")
 	var s server
 	return &s
 }
 
-var todoCollection db.Collection
+var dbCollectionTodo db.Collection
 
 // server is used to implement todo.TodoServiceServer.
 type server struct {
@@ -26,21 +29,45 @@ type Hateoas struct {
 	links []*todo.Link
 }
 
-func (h *Hateoas) AddHateoas(rel, contenttype, href string, method todo.Link_Method) {
+func (h *Hateoas) AddLink(rel, contenttype, href string, method todo.Link_Method) {
 	self := todo.Link{Rel: rel, Href: href, Type: contenttype, Method: method}
 	h.links = append(h.links, &self)
 }
 
-func (s *server) CreateTodo(context.Context, *todo.CreateTodoRequest) (*todo.CreateTodoResponse, error) {
-	return &todo.CreateTodoResponse{Id: "33"}, nil
+func (s *server) CreateTodo(ctx context.Context, req *todo.CreateTodoRequest) (*todo.CreateTodoResponse, error) {
+	id := GenerateULID().String()
+	var item todo.Todo
+	item.Id = id
+	item.Title = req.Item.Title
+	item.Description = req.Item.Description
+	item.Completed = false
+	_, err := dbCollectionTodo.Insert(&item)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "Could not create entity: %s", err)
+	}
+	var h Hateoas
+	h.AddLink("self", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_GET)
+	h.AddLink("delete", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_DELETE)
+	h.AddLink("update", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_PATCH)
+	// todo Proto CreateTodoResponse anpassen
+	return &todo.CreateTodoResponse{Id: id}, nil
+}
+
+func (s *server) DeleteTodo(context.Context, *todo.DeleteTodoRequest) (*todo.DeleteTodoResponse, error) {
+	panic("implement me")
+}
+
+func (s *server) UpdateTodo(context.Context, *todo.UpdateTodoRequest) (*todo.UpdateTodoResponse, error) {
+	panic("implement me")
 }
 
 func (s *server) GetTodo(ctx context.Context, req *todo.GetTodoRequest) (*todo.TodoEntity, error) {
 	var item todo.Todo
-	res := todoCollection.Find(db.Cond{"id": req.Id})
+	res := dbCollectionTodo.Find(db.Cond{"id": req.Id})
 	if err := res.One(&item); err != nil {
 		return nil, status.Errorf(codes.NotFound, "Could not retrieve entity from the database: %s", err)
 	}
+
 	entity := makeTodoEntity(item)
 	return &entity, nil
 }
@@ -55,12 +82,16 @@ func (s *server) ListTodo(ctx context.Context, req *todo.ListTodoRequest) (*todo
 	if req.Page > 0 {
 		page = uint(req.Page)
 	}
-	// todo pagination mit hateoas methode
+	// todo pagination als methode generalisieren
 	nextPage := strconv.FormatUint(uint64(page+1), 10)
 	currentPage := strconv.FormatUint(uint64(page), 10)
 	prevPage := strconv.FormatUint(uint64(page-1), 10)
 
-	res := todoCollection.Find().Paginate(uint(limit))
+	res := dbCollectionTodo.Find().Paginate(uint(limit))
+
+	totalNumberOfPages, _ := res.TotalPages()
+	lastPage := strconv.FormatUint(uint64(totalNumberOfPages), 10)
+
 	if err := res.Page(uint(page)).All(&items); err != nil {
 		return nil, status.Errorf(codes.NotFound, "Could not retrieve item from the database: %s", err)
 	}
@@ -70,27 +101,30 @@ func (s *server) ListTodo(ctx context.Context, req *todo.ListTodoRequest) (*todo
 		collection = append(collection, &entity)
 	}
 	var h Hateoas
-	h.AddHateoas("self", "application/json", "http:localhost:8080/todos?page="+currentPage, todo.Link_GET)
+	h.AddLink("self", "application/json", "http:localhost:8080/todos?page="+currentPage, todo.Link_GET)
 	if page > 1 {
-		h.AddHateoas("prev", "application/json", "http:localhost:8080/todos?page="+prevPage, todo.Link_GET)
+		h.AddLink("prev", "application/json", "http:localhost:8080/todos?page="+prevPage, todo.Link_GET)
 	}
-	h.AddHateoas("next", "application/json", "http:localhost:8080/todos?page="+nextPage, todo.Link_GET)
+	if page < totalNumberOfPages {
+		h.AddLink("next", "application/json", "http:localhost:8080/todos?page="+nextPage, todo.Link_GET)
+	}
+	h.AddLink("first", "application/json", "http:localhost:8080/todos?page=1", todo.Link_GET)
+	h.AddLink("last", "application/json", "http:localhost:8080/todos?page="+lastPage, todo.Link_GET)
 	return &todo.TodoCollection{Data: collection, Links: h.links}, nil
 }
 
 func makeTodoEntity(item todo.Todo) todo.TodoEntity {
 	var h Hateoas
-	h.AddHateoas("self", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_GET)
-	h.AddHateoas("delete", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_DELETE)
-	h.AddHateoas("update", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_PATCH)
+	h.AddLink("self", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_GET)
+	h.AddLink("delete", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_DELETE)
+	h.AddLink("update", "application/json", "http:localhost:8080/todos/"+item.Id, todo.Link_PATCH)
 	entity := todo.TodoEntity{Data: &item, Links: h.links}
 	return entity
 }
 
-func (s *server) DeleteTodo(context.Context, *todo.DeleteTodoRequest) (*todo.DeleteTodoResponse, error) {
-	panic("implement me")
-}
-
-func (s *server) UpdateTodo(context.Context, *todo.UpdateTodoRequest) (*todo.UpdateTodoResponse, error) {
-	panic("implement me")
+func GenerateULID() ulid.ULID {
+	t := time.Now().UTC()
+	entropy := rand.New(rand.NewSource(t.UnixNano()))
+	newID, _ := ulid.New(ulid.Timestamp(t), entropy)
+	return newID
 }
